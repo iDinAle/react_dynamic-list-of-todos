5650db88492dc784791e26e7f96801c91add2ba1
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-19 20:13:01 +0200

    Updates

---
+++ precache-manifest.29edca4a4c4e6aa6ea582e93e18775a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df134f0d7ccb487cac7f627de0003376",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "7edeee44f4ccc50e6517",
+    "url": "/react_dynamic-list-of-todos/static/css/main.e1175a0a.chunk.css"
+  },
+  {
+    "revision": "a93dcb5f4871ee723658",
+    "url": "/react_dynamic-list-of-todos/static/js/2.a2bac020.chunk.js"
+  },
+  {
+    "revision": "7edeee44f4ccc50e6517",
+    "url": "/react_dynamic-list-of-todos/static/js/main.7ef38436.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
